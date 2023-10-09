3fefafe6a7abec460bf04ad50a0fcfd6fbdef60f
Ermin Husic <Ermin.Husic@live.se>
2023-10-09 08:53:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Bankomaten
+{
+    internal class Program
+    {
+        static string[] Användare = { "Ermin", "Christian", "Markus", "Shahram", "Fredrik" };
+        static string[] PinKod = { "1111", "2222", "3333", "4444", "5555" };
+        static string[] KontoÄgare = { "Ermin", "Christian", "Markus", "Shahram", "Fredrik" };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----> Välkommen till bankomaten <----");
+
+            bool Inloggad = false;
+            int InloggningsFörsök = 0;
+
+            while (!Inloggad) 
+            {
+                Console.WriteLine("Ange användarnamn:");
+                string AnvändarNamn = Console.ReadLine();
+
+                Console.WriteLine("Ange pinkod:");
+                string AnvändarPin = Console.ReadLine();
+                
+            }
+        }
+    }
+}
