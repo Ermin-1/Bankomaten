d824e50c32aa5153aea389afa7ffd32e14c34368
Ermin Husic <Ermin.Husic@live.se>
2023-10-09 09:31:18 +0200

    Skapat statiska arrayer för användare och påbörjat programmet i main. Försök till att skapa metod som kontrollerar inloggningsförsök

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace Bankomaten
 {
     internal class Program
-    {
+    {  // Skapat användare och pinkoder
         static string[] Användare = { "Ermin", "Christian", "Markus", "Shahram", "Fredrik" };
         static string[] PinKod = { "1111", "2222", "3333", "4444", "5555" };
         static string[] KontoÄgare = { "Ermin", "Christian", "Markus", "Shahram", "Fredrik" };
@@ -23,5 +23,17 @@
                 
             }
         }
+    }
+    static bool InloggningsFörsök(string användare, string pinKod)
+    {
+        for (int i = 0; i < Användare.Length; i++);
+        {
+            if (Användare[i] == användare && Pinkod[i] == pinKod)
+            {
+                return true;
+            }
+
+        }
+        return false; 
     }
 }
